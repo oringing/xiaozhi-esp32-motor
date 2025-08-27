c50d8af2c5099bfd0a07f358135848e0091700ab
oranging <3033613378@qq.com>
2025-08-28 01:33:33 +0800

    PWM调速功能完成，增加引脚宏定义，减少串口占用等

--- main/boards/bread-compact-wifi/motor_control.h
+++ main/boards/bread-compact-wifi/motor_control.h
@@ -12,13 +12,33 @@
 #include "driver/gpio.h"
 #include "motor/motor.h"
 
-// 电机控制GPIO宏定义
-#define MOTOR_RIGHT_FORWARD GPIO_NUM_11 // 右侧电机正转引脚
-#define MOTOR_RIGHT_BACKWARD GPIO_NUM_12 // 右侧电机反转引脚
-#define MOTOR_LEFT_FORWARD  GPIO_NUM_13  // 左侧电机正转引脚
-#define MOTOR_LEFT_BACKWARD GPIO_NUM_14  // 左侧电机反转引脚
-#define MOTOR_RIGHT_PWM GPIO_NUM_9  // 右侧PWM引脚
-#define MOTOR_LEFT_PWM GPIO_NUM_10  // 左侧PWM引脚
+
+// ESP32-S3-DevKitC-1 支持的PWM引脚列表（仅限此开发板）
+//44针开发板：GPIO 0-21，33-48（包含边界0、21、33、48）
+//4、5、6麦克风占用，7、15、16功放占用，38、39音量加减占用，41、42引脚0.91寸、0.96寸屏幕占用
+#define VALID_PWM_PINS \
+    { \
+        GPIO_NUM_0,  GPIO_NUM_1,  GPIO_NUM_2,  GPIO_NUM_3, \
+        GPIO_NUM_8,  GPIO_NUM_9,  GPIO_NUM_10, GPIO_NUM_11, \
+        GPIO_NUM_12, GPIO_NUM_13, GPIO_NUM_14, \
+        GPIO_NUM_17, GPIO_NUM_18, GPIO_NUM_19, \
+        GPIO_NUM_21, GPIO_NUM_33, GPIO_NUM_34, GPIO_NUM_35, \
+        GPIO_NUM_36, GPIO_NUM_37, \
+        GPIO_NUM_40, GPIO_NUM_43, \
+        GPIO_NUM_44, GPIO_NUM_45, GPIO_NUM_46, GPIO_NUM_47, \
+        GPIO_NUM_48 \
+    }
+
+// 电机控制GPIO宏定义，从上面列觉的合法且未被占用的GPIO引脚中选择
+//电机1、2，三个引脚在ESP32S3开发板上邻近
+#define MOTOR_RIGHT_PWM GPIO_NUM_8  // 右侧PWM引脚
+#define MOTOR_RIGHT_FORWARD GPIO_NUM_3 // 右侧电机正转引脚
+#define MOTOR_RIGHT_BACKWARD GPIO_NUM_46 // 右侧电机反转引脚
+
+// 电机3、4，三个引脚在ESP32S3开发板上邻近
+#define MOTOR_LEFT_PWM GPIO_NUM_11  // 左侧PWM引脚
+#define MOTOR_LEFT_FORWARD  GPIO_NUM_12  // 左侧电机正转引脚
+#define MOTOR_LEFT_BACKWARD GPIO_NUM_13  // 左侧电机反转引脚
 
 class MotorControl {
  public:
